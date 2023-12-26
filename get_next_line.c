d7ba52a32906d0c1bbb7e7105fe220d5b2f42cad
rda-cunh <ricard.mend@gmail.com>
2023-12-26 12:49:52 +0000

    new

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 19:04:54 by rda-cunh          #+#    #+#             */
-/*   Updated: 2023/12/18 19:21:48 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2023/12/26 12:49:42 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 char	*get_next_line(int fd)
 {
-	
+	int	bytes_read;
+
+	bytes_read = read(fd, cup_buffer, 3);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 19:05:51 by rda-cunh          #+#    #+#             */
-/*   Updated: 2023/12/18 19:17:54 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2023/12/26 12:38:36 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+#include <unistd.h>
 
 char	*get_next_line(int fd);
 

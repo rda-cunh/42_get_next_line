7f19eaf7b215a99d250542c1460d7de4c3844350
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2024-01-12 14:02:44 +0000

    new

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,44 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 19:04:54 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/01/11 18:49:30 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/12 14:00:32 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+
+char	*append_buffer(char *basin_buffer, char *read_buffer)
+
+
+
 //Function to read data from the file and create partial content. 
 
 static char	*read_from_file(static char *basein_buffer, int fd)
 {
 	int			bytes_read;
-	char		*buffer;
-	static int	count = 1;
-
-	printf("ft_calloc#[%d]---", count++);
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	if (buffer == NULL)
+	char		*cup_buffer;
+	
+	cup_buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (cup_buffer == NULL)
 		return (NULL);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
+	bytes_read = 1; 
+	while (bytes_read > 0)
 	{
-		free(buffer);
-		return (NULL);
+		bytes_read = read(fd, cup_buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(cup_buffer);
+			return (NULL);
+		}
+		cup_buffer[bytes_read] = '\0';
+		basein_buffer = append_buffer(basein_buffer, cup_buffer);
+		if (ft_strchr(basein_buffer, '\n'))
+			break ;
 	}
-	return (buffer);
+	free (cup_buffer);
+	return (basein_buffer);
 }
 
 char	*get_next_line(int fd)

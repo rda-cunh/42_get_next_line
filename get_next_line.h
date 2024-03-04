adbe511a67bc9adc899a2acea3b4f91fc4af7ac5
rda-cunh <ricard.mendes@gmail.com>
2024-03-04 23:56:17 +0000

    new

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 19:05:51 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/02/25 10:41:02 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/03/04 23:36:25 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
+# include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 3
+#  define BUFFER_SIZE 32
 # endif
 
 char	*get_next_line(int fd);
-void	*ft_calloc(size_t nmemb, size_t size);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-char	*extract_line(char *buffer);
-char	*obtain_remaining(char *buffer);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t	strlen_at(const char *s, int c);
+char	*cpy_buffer(const char *s, size_t n);
+char	*find_chr(const char *s, int c);
+char	*merge_previous_and_current(char *s1, char const *s2);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,68 +6,21 @@
 /*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 19:06:00 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/02/25 10:40:08 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/03/04 23:56:06 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*extract_line(char *buffer)
+size_t	strlen_at(const char *s, int avoid)
 {
-	int		i;
-	char	*line;
+	size_t	i;
 
 	i = 0;
-	while (buffer[i] != '\0' && buffer[i] != '\n')
+	if (!s)
+		return (0);
+	while (s[i] && s[i] != avoid)
 		i++;
-	if (!(line = (char *)malloc(i + 1)))
-		return (NULL);
-	i = 0;
-	while (buffer[i] != '\0' && buffer[i] != '\n')
-	{
-		line[i] = buffer[i];
-		i++;
-	}
-	line[i] = '\0';
-	return (line);
-}
-
-char	*obtain_remaining(char *buffer)
-{
-	int		i;
-	int		j;
-	char	*remaining;
-
-	i = 0;
-	while (buffer[i] != '\0' && buffer[i] != '\n')
-		i++;
-	if (buffer[i] == '\0')
-		return (NULL);
-	i++;
-	j = 0;
-	while (buffer[i + j] != '\0')
-		j++;
-	if (!(remaining = (char *)malloc(j + 1)))
-		return (NULL);
-	j = 0;
-	while (buffer[i + j] != '\0')
-	{
-		remaining[j] = buffer[i + j];
-		j++;
-	}
-	remaining[j] = '\0';
-	return (remaining);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
 	return (i);
 }
 
@@ -90,70 +43,35 @@ void	*ft_memcpy(void *dst, const void *src, size_t len)
 	return (dst);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*find_chr(const char *buffer, int to_find)
 {
-	char	*str;
-	size_t	s1_len;
-	size_t	s2_len;
-
-	if (!s1 || !s2)
+	if (!buffer)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
-	if (!str)
-		return (NULL);
-	ft_memcpy(str, s1, s1_len);
-	ft_memcpy(str + s1_len, s2, s2_len);
-	str[s1_len + s2_len] = '\0';
-	return (str);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	char	altc;
-	char	*alts;
-
-	altc = c;
-	alts = (char *)s;
-	while (*alts != '\0')
+	while (*buffer)
 	{
-		if (*alts == altc)
-			return (alts);
-		alts++;
+		if (*(unsigned char *)buffer == (unsigne char)to_find)
+			return ((char *)buffer);
+		buffer++;
 	}
-	if (altc == 0)
-		return (alts);
+	if (*(unsigned char *)buffer == (unsigned char)to_find);
+		return ((char *)buffer);
 	return (NULL);
 }
 
-static void	*ft_memset(void *s, int c, size_t len)
+char *cpy_buffer(const char *buffer, size_t len)
 {
-	size_t			i;
-	unsigned char	*tmp;
+	char	*cpy;
+	size_t	i;
 
-	tmp = (unsigned char *)s;
+	cpy = (char *) malloc(len + 1);
+	if (!cpy)
+		return (NULL);
 	i = 0;
-	while (i < len)
+	while (buffer[i] && i < len)
 	{
-		tmp[i] = c;
+		cpy[i] = buffer[i];
 		i++;
 	}
-	return (s);
-}
-
-static void	ft_bzero(void *b, size_t len)
-{
-	ft_memset(b, 0, len);
-}
-
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*p;
-
-	p = malloc(nmemb * size);
-	if (p == 0)
-		return (NULL);
-	ft_bzero(p, nmemb * size);
-	return (p);
+	cpy[i] = '\0';
+	return (cpy);
 }
